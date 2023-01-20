1a7c88129eae0ed43df35c321f0ee580fa0fc937
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-20 13:45:16 +0100

    Input, Button, Options added

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -1,11 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { FC } from 'react'
 
 const AddTaskForm:FC = () => {
 
   return (
 
-    <div>AddTaskForm</div>
-    
+        <form style={{ minHeight: '20vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+            <input type="text" placeholder='Add your Task...' className='add-container-input'  style={{ border: 'none', outline: 'none', fontSize: '1.5rem', fontFamily: 'Indie Flower', background: 'none', color: '#F7F9F9', }} />
+            
+            <button type='submit' className='add-container-button' >Add</button>
+
+            <div>
+
+                <select name="todos" >
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="incomplete">Incomplete</option>
+                </select>
+
+            </div>
+
+        </form>
+
   )
 }
 
