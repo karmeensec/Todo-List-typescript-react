1308669b434647ba0eaf03800caf2a01c32f14d7
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-20 13:41:37 +0100

    Header Stying added

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import AddTaskForm from './components/AddTaskForm'
 
 const App: FC = () => {
 
-  return (
+  return <div >
 
-    <div>
-
-      <h1>Your To Do List</h1>
-
+      <header style={{ minHeight: '20vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <h3 style={{  fontSize: '2.5rem', fontFamily:  'Gloria Hallelujah', color: '#F7F9F9',}}>Your To Do List</h3>
+      </header>
+      
       <AddTaskForm />
 
     </div>
 
-  )
+  
 }
 
 export default App
