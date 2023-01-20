7f09557f960597126c2b17b7a6884fc5228d0ea9
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-21 00:24:17 +0100

    mapping through todos

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import ToDo from './ToDo'
 
+
 const ToDoList = ({ todos }: any) => {
 
   return (
@@ -8,7 +9,9 @@ const ToDoList = ({ todos }: any) => {
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
       <ul style={{ listStyle: 'none', fontSize: '1.5rem', fontFamily: 'Indie Flower', color: '#F7F9F9', minWidth: '50vh', }}>
-
+          {todos.map( (todo: any) => ( 
+            <ToDo />
+          ))}
       </ul>
       
     </div>
