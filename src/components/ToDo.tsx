1dab30462b8eaa3aeab3851106a44e4350604d53
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-21 13:45:53 +0100

    Complete tasks event handler function added

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -10,12 +10,28 @@ const ToDo = ({ text, todos, setTodos, todo}: any) => {
     
   }
 
+  const ToDoCompleteHandler = function(): void {
+
+    setTodos(todos.map((t: any) => {
+
+      if (t.id === todo.id) {
+
+        return {...t,  completed: !t.completed}
+
+      }
+
+      return t;
+
+    }))
+
+  }
+
   return (
 
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', transition: '1s ease-in-out'  }} >
 
         <li style={{ padding: '1rem' }}> {text} </li>
-        <button type='submit' className='complete-button'> <VscCheck /> </button>
+        <button type='submit' className='complete-button' onClick={ToDoCompleteHandler}> <VscCheck /> </button>
         <button type='submit' className='trash-button' onClick={ToDoDeleteHandler}> <VscTrash /> </button>
 
     </div>
