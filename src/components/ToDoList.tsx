8521ce09e1523b380cc5ebc77fe0f95870acb43e
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-21 19:04:06 +0100

    ToDos filtered by categories

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,14 +2,14 @@ import React, { FC } from 'react'
 import ToDo from './ToDo'
 
 
-const ToDoList = ({ todos, setTodos }: any) => {
+const ToDoList = ({ todos, setTodos, toDoFilter }: any) => {
 
   return (
 
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
       <ul style={{ listStyle: 'none', fontSize: '1.5rem', fontFamily: 'Indie Flower', color: '#F7F9F9', minWidth: '50vh', }}>
-          {todos.map( (todo: any) => ( 
+          {toDoFilter.map( (todo: any) => ( 
             <ToDo text = {todo.text} id = {todo.id} key={todo.id} todos = {todos} setTodos = {setTodos} todo={todo} />
           ))}
       </ul>
