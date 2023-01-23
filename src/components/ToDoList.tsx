b0f8edb7b025e37d527fe046c98543be66253276
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-23 23:25:32 +0100

    Final Changes

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -6,7 +6,7 @@ const ToDoList = ({ todos, setTodos, toDoFilter }: any) => {
 
   return (
 
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
 
       <ul style={{ listStyle: 'none', fontSize: '1.5rem', fontFamily: 'Indie Flower', color: '#F7F9F9', minWidth: '50vh', }}>
           {toDoFilter.map( (todo: any) => ( 
